1e3d152554534122b273d1b57cf7d1f3f33eee7a
IvankovCL <ivankovfa@mail.ru>
2016-10-26 21:48:59 +0300

    minor changes

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-var toggle = function() {
-  var mydiv = document.getElementById('demoForm');
-  if (mydiv.style.display === 'block' || mydiv.style.display === '')
-    mydiv.style.display = 'none';
-  else
-    mydiv.style.display = 'block'
-  }
