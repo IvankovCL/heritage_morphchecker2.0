9020d2757c707366cf6dd91575f5cd1fd2517298
IvankovCL <ivankovfa@mail.ru>
2017-04-17 17:02:24 +0300

    webapp

--- static/js/without_polling.js
+++ static/js/without_polling.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-	document.title = 'Morphchecker';
+    document.title = 'Morphchecker';
     $('#demoButton').click(function () {
          $(this).hide();
          $('#demoForm').show();
-    });    
-    $('#demoForm').hide();    
-    $("#txtInput").focus(function () {
-        $('#txtOutput').val('')
-    });    
+    });
+    $('#demoForm').hide();
+    if ($("#textInput").val() == '') {
+        document.getElementById("textOutput").innerHTML = '';
+    }
     $("#send").click(function() {
-        $.post('/data', $('#txtInput').val(), function(data) {
-            $('#txtOutput').val(data.result)
+        $.post('/data', $('#textInput').val(), function(data) {
+            document.getElementById("textOutput").innerHTML = data.result
         });
-    });     
+    });
 });
